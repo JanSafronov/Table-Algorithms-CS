cbfdb3c9d9488865349c6358e62c771dadbc6729
Pomidorka1234 <Jansafronov@gmail.com>
2021-02-14 23:32:17 +0200

    first commit

---
+++ NaryTree.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Diagnostics;
+using System.Globalization;
+
+
+namespace Table_Efficiency_C
+{
+    delegate void TreeVisitor<T>(T nodeData);
+
+    public class NaryTree<T>
+    {
+        private T value;
+        private LinkedList<NaryTree<T>> node;
+
+        /// <summary>Tree Data Class initiation with valued root and integer amount nodes of valued type T</summary>
+        /// <param value="value">The value of type T in the root of the tree</param>
+        /// <param node="node">Linked List of Tree Data Class node of the root</param>
+        public NaryTree(T value, LinkedList<NaryTree<T>> node) {
+            this.value = value;
+            this.node = node;
+        }
+
+        /// <summary>Tree Data Class initiation with valued root and nulled node </summary>
+        /// <param value="value">The value of type T in the root of the tree</param>
+        /// <param node="node">null node of the root</param>
+        public NaryTree(T value) {
+            this.value = value;
+            this.node = null;
+        }
+
+
+
+        /// <summary>Value getter</summary>
+        /// <summary>Value setter</summary>
+        public T Value { get => this.value; set => this.value = value; }
+
+
+
+        /// <summary>Node List getter</summary>
+        /// <summary>Node List setter</summary>
+        public LinkedList<NaryTree<T>> NodeList { get => this.node; set => this.node = value; }
+
+
+
+        public NaryTree<T> getNode(NaryTree<T> target) => this.node.Find(target).Value;
+        public NaryTree<T> setNote(NaryTree<T> target, NaryTree<T> node) => this.node.Find(target).Value = node;
+
+
+
+        /// <summary>Check for an existent node</summary>
+        public bool hasNode() {
+            return !this.node.Contains(null);
+        }
+
+
+        
+        /// <summary>Append a new linked tree after a node</summary>
+        /// <param name="node">Linked tree to append</param>
+        /// <typeparam name="node">des</typeparam>
+        public void appendNode(LinkedListNode<NaryTree<T>> node, LinkedListNode<NaryTree<T>> target) {
+            this.node.AddAfter(target, node);
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+System.AppDomain
+namespace Table_Efficiency__C__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int n = 2;
+        }
+    }
+}
